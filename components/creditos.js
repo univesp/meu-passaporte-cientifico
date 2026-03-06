3f7268b88a4ea5b7046a8b2c65036bceee6b8c52
GalloGabriel <gabriel.gallo2242@gmail.com>
2026-03-06 11:07:46 -0300

    Atualizando infos dos créditos no rodapé

--- components/creditos.js
+++ components/creditos.js
@@ -11,13 +11,13 @@ let creditosContent = `
       <p><span class="funcao">Diretora Acadêmica</span>Romero Tori</p>
 
       <h6>Equipe</h6>
-      <p><span class="funcao">Docente responsável</span>Fulano de Tal</p>
-      <p><span class="funcao">Coordenação</span>Wesley de Souza Lima</p>
-      <p><span class="funcao">Roteirização e Design Instrucional</span>Fulano de Tal, Ciclano de Tal</p>
-      <p><span class="funcao">Ilustração</span>Fulano de Tal, Ciclano de Tal</p>
-      <p><span class="funcao">Layout</span>Fulano de Tal, Ciclano de Tal</p>
-      <p><span class="funcao">Programação</span>Fulano de Tal, Ciclano de Tal</p>
-      <p><span class="funcao">Revisão</span>Fulano de Tal, Ciclano de Tal</p>
+      <p><span class="funcao">Docente responsável</span>Willian José Ferreira</p>
+      <p><span class="funcao">Supervisora</span>Taitiâny Karita Bonzanini Minetto</p>
+      <p><span class="funcao">Coordenadora</span>Sheila Regina Brisson Ruiz</p>
+      <p><span class="funcao">Designer Instrucional</span>Juliana Quitério Lopez Salvaia</p>
+      <p><span class="funcao">Roteirização</span>Willian José Ferreira e Juliana Quitério Lopez Salvaia</p>
+      <p><span class="funcao">Arte</span>Alex Fernandes</p>
+      <p><span class="funcao">Programação</span>Gabriel Henrique Gallo da Silva, Debora Lais Vieira T. do Nascimento e Jose Roberto Candido da Silva</p>
 
     </div>
     <div class="modal-footer">
