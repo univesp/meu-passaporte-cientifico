68c6f8a6c1f4b0f378816407b48e55728e39074a
GalloGabriel <gabriel.gallo2242@gmail.com>
2026-04-28 12:45:52 -0300

    Correção dos botões de compartilhamento do JsSocials e inicio do ajuste media query até 576px

--- javascripts/events-components.js
+++ javascripts/events-components.js
@@ -1,5 +1,32 @@
 /* Inserindo conteudos de componentes */
 
+  // Função para inicializar os botões de compartilhamento
+  function initSharing() {
+    if (typeof $.fn.jsSocials !== 'undefined') {
+      $(".sharing").jsSocials({
+        shares: [
+          {
+            share: "facebook",
+            logo: "fab fa-facebook-f",
+          },
+          {
+            share: "twitter",
+            logo: "fab fa-twitter",
+          },
+          {
+            share: "whatsapp",
+            logo: "fab fa-whatsapp",
+          },
+        ],
+        url: window.location.href,
+        text: 'Confira o REA ' + $(document).attr('title') + '.',
+        showLabel: false,
+        showCount: false,
+        shareIn: "popup",
+      });
+    }
+  }
+
   // Créditos
   const creditosContainer = document.querySelector('#creditos');
   creditosContainer.innerHTML = creditosContent;
@@ -7,3 +34,7 @@
   // Footer
   const footerContainer = document.querySelector('#footer');
   footerContainer.innerHTML = footerContent;
+
+
+  // Inicializa os botões de compartilhamento APÓS inserir o footer
+  initSharing();
